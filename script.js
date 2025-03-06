b74f6e85235271362b757488ca53bb1d62636b53
luongnguyeen <147536458+luongnguyeen@users.noreply.github.com>
2025-03-06 19:13:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -53,8 +53,8 @@ document.addEventListener("DOMContentLoaded", function() {
       // Tạo phần tử ảnh với src giống ảnh được click
       const enlargedImg = document.createElement("img");
       enlargedImg.src = this.src;
-      enlargedImg.style.maxWidth = "60vw"; // Giảm kích thước ảnh phóng to
-      enlargedImg.style.maxHeight = "60vh";
+      enlargedImg.style.maxWidth = "30vw"; // Giảm kích thước ảnh phóng to
+      enlargedImg.style.maxHeight = "30vh";
       enlargedOverlay.appendChild(enlargedImg);
       document.body.appendChild(enlargedOverlay);
       // Dừng animation quay của cube
